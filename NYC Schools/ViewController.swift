b7c38220c74e21f3cbd27dc2cb1edce434abde23
wanaya <memoanaya@gmail.com>
2022-12-03 21:34:11 -0800

    Initial Commit

---
+++ NYC Schools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYC Schools
+//
+//  Created by Guillermo Anaya on 03/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
